f02b93a993fa70e2dd055fcf0d9a6ff2258f9eaa
Christiano0407 <placebo0704@gmail.com>
2023-05-18 10:05:25 -0600

    add new functios with events

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,15 +1,37 @@
 //**! ============================= Component Transaction Form */
+import { useState } from 'react';
 
 export const Transaction = () => {
+  const [description, setDescription] = useState();
+
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="w-full flex justify-center my-5">
-      <form className="w-1/2 p-5 m-[20px, auto] flex justify-center items-center flex-col border-4 border-sky-950">
+      <form
+        onSubmit={onSubmit}
+        className="w-1/2 p-5 m-[20px, auto] flex justify-center items-center flex-col border-4 border-sky-950"
+      >
         <input
           type="text"
           placeholder="Enter your description"
-          className="p-3 text-base text-black font-semibold w-1/2"
+          className="p-3 text-lg text-black font-semibold w-1/2 my-3"
+          onChange={(e) => setDescription(e.target.value)}
         />
-        <input type="number" placeholder="00:00" className="p-3 w-1/2 my-3 " />
+        <input
+          type="number"
+          step="0.01"
+          placeholder="00:00"
+          className="p-3 w-1/2 my-3 text-lg text-black "
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button className="p-4 bg-zinc-900 text-white font-medium text-base my-2 rounded-md">
+          Add Transaction
+        </button>
       </form>
     </section>
   );
